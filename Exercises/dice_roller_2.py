1551dc285e5c45a7925a30c39caaa39425110738
Maria Jimenez <mjimenez.intern@veridas.com>
2025-05-14 13:41:49 +0200

    dice roller program

---
+++ Exercises/dice_roller_2.py
@@ -0,0 +1,44 @@
+import random
+
+# print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518")
+# ● ┌ ─ ┐ │ └ ┘ estos son los simbolos que pintan los códigos numéricos de la línea anterior
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+
+for line in range(5): # bucle para cada línea de la ficha
+    for key in range(1, 7): # bucle para cada ficha (de la 1 a la 6)
+        print(dice_art[key][line], end=" ")
+    print()
+
