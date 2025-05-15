b9ab94ca4026af685f08d98b516b34d96f438ae1
Maria Jimenez <mjimenez.intern@veridas.com>
2025-05-15 10:18:39 +0200

    iterables

---
+++ Lessons/iterables.py
@@ -0,0 +1,6 @@
+# Iterables = son objetos/colecciones que pueden devolver sus elementos
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+for key, value in my_dictionary.items():
+    print(key, value)
